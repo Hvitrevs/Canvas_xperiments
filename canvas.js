c01ba18d2f5237f8160e24ce4a24d5cc3a9a4a7a
Hvitrevs <venusof12pm@proton.me>
2023-10-18 14:01:16 -0400

    mouse response

--- canvas.js
+++ canvas.js
@@ -19,4 +19,45 @@ addEventListener("mousemove", function (event) {
 addEventListener("resize", function () {
 	canvas.width = innerWidth;
 	canvas.height = innerHeight;
+  init();
 });
+
+class Object {
+  constructor (x, y, radius, color){ 
+	this.x = x;
+	this.y = y;
+	this.radius = radius;
+	this.color = color;
+  } 
+
+	draw() {
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		c.fillStyle = this.color;
+		c.fill();
+		c.closePath();
+	};
+}
+// Implementation
+let object;
+function init() {
+  object = [];
+
+  for (let i = 0; i < 190; i++) {
+
+  }
+
+}
+
+
+function animate() {
+	requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillText('canvas', mouse.x, mouse.y)
+
+    particles.forEach(particle => {
+      particle.update();
+  });
+}
+init(); 
+animate();
