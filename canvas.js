071bff8076d46412fa3df19334a352dd90eacf3f
Hvitrevs <venusof12pm@proton.me>
2023-10-18 13:54:29 -0400

    start over

--- canvas.js
+++ canvas.js
@@ -1,15 +1,22 @@
-import * as dat from 'dat.gui'
+
 
 const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const  c = canvas.getContext('2d');
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-c.beginPath();
-c.moveTo(0, canvas.height / 2);
-for (let i =0; i < canvas.width; i++) {
-  c.lineTo(i, canvas.height / 2 + Math.sin(i * 0.01) * 100);
-}
+const mouse = {
+	x: innerWidth / 2,
+	y: innerHeight / 2
+};
 
-c.stroke();
+addEventListener("mousemove", function (event) {
+	mouse.x = event.clientX;
+	mouse.y = event.clientY;
+});
+
+addEventListener("resize", function () {
+	canvas.width = innerWidth;
+	canvas.height = innerHeight;
+});
