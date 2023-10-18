49fab524131e97b2e55f2daeab412dfdb6163609
Hvitrevs <venusof12pm@proton.me>
2023-10-18 15:13:24 -0400

    error was bc I didnt call function facepalm facepalm

--- canvas.js
+++ canvas.js
@@ -23,11 +23,12 @@ addEventListener("resize", function () {
 });
 
 class Particle {
-  constructor (x, y, radius, color){ 
+  constructor (x, y, radius, color, velocity) { 
 	this.x = x;
 	this.y = y;
 	this.radius = radius;
 	this.color = color;
+  this.velocity = velocity;
   } 
 
 	draw() {
@@ -37,31 +38,63 @@ class Particle {
 		c.fill();
 		c.closePath();
 	};
-  update(){
-    this.draw();
+  update() {
+    this.draw()
+    this.x += this.velocity.x
+    this.y += this.velocity.y
   }
 }
 
 
 // Implementation
-let particles;
+let particles
+
 function init() {
   particles = [];
+  const radius = 130
 
-  for (let i = 0; i < 190; i++) {
-    particles.push(new Particle( canvas.width / 2, canvas.height / 2, 5, 'blue'))
-  }
+  for (let i = 0; i < 30; i++) {
+    // if full circle = pi * 2 radians
+    const radian = (Math.PI * 2) / 30;
+    const x = canvas.width  / 2;
+    const y = canvas.height / 2;
+    particles.push(
+      new Particle( x, y , 5, 'blue', {
+      x: Math.cos(radian * i),
+      y: Math.sin(radian * i)
+      })
+    )
+  };
 
 }
 
+function generateRing() {
+  setTimeout(generateRing, 1000)
+  for (let i = 0; i < 30; i++) {
+    // if full circle = pi * 2 radians
+    const radian = (Math.PI * 2) / 30;
+    const x = canvas.width  / 2;
+    const y = canvas.height / 2;
+    particles.push(
+      new Particle( x, y , 5, 'blue', {
+      x: Math.cos(radian * i),
+      y: Math.sin(radian * i)
+      })
+    )
+  };
+}
 
 function animate() {
 	requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
 
     particles.forEach(particle => {
-      particle.draw();
+      particle.update();
   });
 }
 init(); 
 animate();
+
+
+// const x = canvas.width  / 2 + Math.cos(radian * i) *  radius;
+// const y = canvas.height / 2 + Math.sin(radian * i) * radius;
