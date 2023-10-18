ec089a686b4032664e5a212f76a83f2a0f556e8e
Hvitrevs <venusof12pm@proton.me>
2023-10-18 13:31:54 -0400

    line with sinus

--- canvas.js
+++ canvas.js
@@ -3,3 +3,11 @@ const c = canvas.getContext('2d');
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+
+c.beginPath();
+c.moveTo(0, canvas.height / 2);
+for (let i =0; i < canvas.width; i++) {
+  c.lineTo(canvas.width, canvas.height / 2);
+}
+
+c.stroke();
