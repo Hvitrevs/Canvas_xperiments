578137a5b037e835bb16276edcc7744c94bc4f5a
Hvitrevs <venusof12pm@proton.me>
2023-10-19 13:42:46 -0400

    mouse properties

--- canvas.js
+++ canvas.js
@@ -12,8 +12,15 @@ const mouse = {
 };
 
 addEventListener('mousemove', function (event) {
-	mouse.x = event.clientX;
-	mouse.y = event.clientY;
+	mouse.x = event.x;
+	mouse.y = event.y;
+  
+});
+
+addEventListener('click', function(event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  
 });
 
 addEventListener('resize', function () {
@@ -25,11 +32,28 @@ addEventListener('resize', function () {
 function drawCircle() {
   c.fillStyle = 'red';
   c.beginPath();
-  c.arc(100, 100, 50, 0, Math.PI * 2);
+  c.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
   c.fill();
 }
 
-drawCircle();
+
+class Particle {
+  constructor() {
+    this.x = mouse.x;
+    this.y = mouse.y;
+    // random between 1 and 6
+    this.size = Math.random() * 5 + 1;
+    // move away from mouse
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+}
+
+
+function animate() {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(animate);
+}
 
 
 
