c918a11766d460411f116ee955c6c85f078b3ff8
Hvitrevs <venusof12pm@proton.me>
2023-10-19 16:14:39 -0400

    adding functions and array

--- canvas.js
+++ canvas.js
@@ -32,9 +32,11 @@ addEventListener('resize', function () {
 
 
 class Particle {
-  constructor() {
-    this.x = mouse.x;
-    this.y = mouse.y;
+  constructor(effect) {
+    this.effect = effect;
+    this.x = Math.random() * this.effect.width;
+    this.y = Math.random() * this.effect.height;
+    this.radius = 15;
 
     // this.x = Math.random() * canvas.width;
     // this.y = Math.random() * canvas.height;
@@ -45,10 +47,34 @@ class Particle {
 
   }
 
-  draw() {
-
+  draw(context) {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fill();
   }
 }
+class Effect {
+  constructor(canvas){
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.particles = [];
+    this.numberOfParticles = 20;
+    this.createParticles();
+  }
+  createParticles(){
+    for(let i =0; i < this.numberOfParticles; i++){
+      this.particles.push(new Particle(this));
+    }
+  }
+
+  handleParticles(){
+    this.particles.forEach(particle => {
+      particle.draw();
+    })
+  }
+}
+
 
 function animate() {
   c.clearRect(0, 0, canvas.width, canvas.height);
